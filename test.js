623dccb1fc673d163f32e1f56984fec7c717cadb
Sineway <sineway@yandex.com>
2021-10-13 11:25:29 +0300

    Change indentation to tabs

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 customElements.define("x-password", class extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({
-            mode: "open"
-        }).innerHTML = `
-            <input type="password" value="qwerty123">
-        `;
-    }
+	constructor() {
+		super();
+		this.attachShadow({
+			mode: "open"
+		}).innerHTML = `
+			<input type="password" value="qwerty123">
+		`;
+	}
 });
