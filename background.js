c85e01e453951dae8f631bcbe1da9a16e12ece3d
Sineway <sineway@yandex.com>
2021-10-13 14:23:10 +0300

    Update context menus
    - Use <Reveal password> label with access key instead of extension name
    - Remove <page>, <frame>, and <browser_action> contexts

--- background.js
+++ background.js
@@ -1,49 +1,4 @@
-let { homepage_url } = browser.runtime.getManifest()
-
-browser.contextMenus.removeAll().then(async () => {
-	let command = (await browser.commands.getAll()).find(command => {
-		return command.name == "view_password"
-	})
-	if (command.shortcut) {
-		command.shortcut = `(${ command.shortcut })`
-	}
-	browser.contextMenus.create({
-		contexts: ["page", "frame", "editable"],
-		title: `${ browser.i18n.getMessage("name") } ${ command.shortcut }`,
-		id: "view_password"
-	})
-	browser.contextMenus.create({
-		contexts: ["browser_action"],
-		title: browser.i18n.getMessage("contributeTranslation"),
-		id: `${ homepage_url }/issues/2`
-	})
-	browser.contextMenus.create({
-		contexts: ["browser_action"],
-		title: browser.i18n.getMessage("contributeBugReport"),
-		id: `${ homepage_url }/issues/new?labels=bug`
-	})
-})
-
-browser.contextMenus.onClicked.addListener(info => {
-	if (info.menuItemId == "view_password") {
-		viewPassword()
-	}
-	else if (info.menuItemId.startsWith(homepage_url)) {
-		browser.tabs.create({
-			url: info.menuItemId
-		})
-	}
-})
-
-browser.commands.onCommand.addListener(name => {
-	if (name == "view_password") {
-		viewPassword()
-	}
-})
-
-browser.browserAction.onClicked.addListener(viewPassword)
-
-async function viewPassword() {
+let revealPassword = async () => {
 	let [tab] = await browser.tabs.query({
 		currentWindow: true,
 		active: true
@@ -51,13 +6,31 @@ async function viewPassword() {
 	browser.tabs.executeScript(tab.id, {
 		file: "content.js",
 		allFrames: true
-	}).catch(error => {
-		console.trace(error.message)
-	})
+	}).catch(
+		console.error
+	)
 }
 
+browser.browserAction.onClicked.addListener(
+	revealPassword
+)
 
+browser.runtime.onInstalled.addListener(() => {
+	browser.contextMenus.create({
+		id: "reveal_password",
+		contexts: ["editable"],
+		title: browser.i18n.getMessage("context_menu")
+	})
+})
 
+browser.contextMenus.onClicked.addListener(info => {
+	if (info.menuItemId == "reveal_password") {
+		revealPassword()
+	}
+})
 
-
-
+browser.commands.onCommand.addListener(name => {
+	if (name == "reveal_password") {
+		revealPassword()
+	}
+})
